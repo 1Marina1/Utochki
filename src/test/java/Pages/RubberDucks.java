d7dc3059f39e1f8b980fd6bfcc4e7470edfa7c78
Marina Veremchuk <veremchuk.marina1@gmail.com>
2022-03-22 17:59:25 +0300

    PageFatory, TestBase

---
+++ src/test/java/Pages/RubberDucks.java
@@ -0,0 +1,38 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class RubberDucks {
+
+    @FindBy(css = "#box-category > nav > a:first-child")
+    private WebElement nameSortingButton;
+
+    @FindBy(css =".filter > span")
+    private WebElement priceSortingButton;
+
+    @FindBy(css = ".price")
+    private List<WebElement> productPrice;
+
+    @FindBy(css = ".name")
+    private List<WebElement> productName;
+
+    private WebDriver driver;
+
+    public void sortByName(){
+        nameSortingButton.click();
+    }
+
+    public void sortByPrice(){
+        priceSortingButton.click();
+    }
+    public List<WebElement> getProductPrice(){
+        return productPrice;
+    }
+    public List<WebElement> getProductName(){
+        return productName;
+    }
+}
